73f7efc5e4290391a8d57a7602f44204f6a4b11c
Enduriel <endur1el@protonmail.com>
2024-01-01 00:50:41 +0100

    refactor: jobify input system

--- Assets/Scripts/Input/Systems/InputSystem.cs
+++ Assets/Scripts/Input/Systems/InputSystem.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Input.Jobs;
 using MyNamespace.Input.Enums;
 using Unity.Collections;
 using Unity.Entities;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Controls;
+using View.Aspects;
 using Ray = Unity.Physics.Ray;
 
 namespace MyNamespace
@@ -17,7 +20,6 @@ namespace MyNamespace
     public partial class InputSystem : SystemBase
     {
         private DefaultMovementActions _movementActions;
-        private Entity _playerEntity;
 
         private NativeHashMap<int, ButtonState> _lastFrameState = new(2, Allocator.Persistent)
         {
@@ -44,7 +46,6 @@ namespace MyNamespace
             ecb.SetComponent(singleton, new UserMouseInfo());
             
             ecb.Playback(EntityManager);
-            _playerEntity = SystemAPI.GetSingletonEntity<UserMouseInfo>();
         }
         // Start is called before the first frame update
         protected override void OnStartRunning()
@@ -55,106 +56,21 @@ namespace MyNamespace
         protected override void OnUpdate()
         {
             var mousePos = Mouse.current.position.ReadValue();
-            // why is it a vector3 and not a vector2?
-            // why is there no way to immediately get an unmanaged one?
-            // or at least easily cast?
-            // who tf knows
             var managedRay = Camera.main!.ScreenPointToRay(new Vector3(mousePos.x, mousePos.y, 0));
-            // todo verify that this actually needs to happen immediately,
-            // pretty sure there's a way it doesn't which would be cleaner
-            var ecb = SystemAPI.GetSingleton<EndSimulationEntityCommandBufferSystem.Singleton>()
-                .CreateCommandBuffer(World.Unmanaged);
-            ecb.SetComponent(_playerEntity, new UserMouseInfo
+            new InputUpdateJob
             {
-                Position = mousePos,
-                Ray = new Ray
-                {
-                    Origin = managedRay.origin,
-                    Displacement = managedRay.direction
-                }
-            });
-
-            UpdateComponentWithValue<UserMovement, UserFloat2InputValue, float2>(ecb, InputType.Move, new UserFloat2InputValue
-            {
-                Value = _movementActions.DefaultMap.Movement.ReadValue<Vector2>()
-            });
-            UpdateComponentWithValue<UserScroll, UserIntInputValue, int>(ecb, InputType.Scroll, new UserIntInputValue
-            {
-                Value = (int)Mouse.current.scroll.ReadValue().normalized.y
-            });
-            UpdateComponentWithValue<UserMouseMovement, UserFloat2InputValue, float2>(ecb, InputType.MouseMove, new UserFloat2InputValue
-            {
-                Value = _movementActions.DefaultMap.MouseMovement.ReadValue<Vector2>()
-            });
-            
-            UpdateComponent<UserSelect>(ecb, InputType.Select, Mouse.current.leftButton);
-            UpdateComponent<UserDrag>(ecb, InputType.Drag, Mouse.current.middleButton);
-        }
-        
-        private void UpdateComponent<T>(EntityCommandBuffer ecb, InputType inputType, ButtonControl button)
-            where T : unmanaged, IComponentData, IUserInput
-        {
-            var value = ButtonState.None;
-            if (button.isPressed)
-            {
-                if ((_lastFrameState[(int)inputType] & ButtonState.PressedOrHeld) == ButtonState.None)
-                    value = ButtonState.Pressed;
-                else
-                    value = ButtonState.Held;
-            }
-            else if ((_lastFrameState[(int)inputType] & ButtonState.PressedOrHeld) != ButtonState.None)
-            {
-                value = ButtonState.Released;
-            }
-            
-            _lastFrameState[(int)inputType] = value;
-            if (value == ButtonState.None)
-            {
-                ecb.RemoveComponent<T>(_playerEntity);
-            }
-            else
-            {
-                ecb.AddComponent<T>(_playerEntity);
-                ecb.SetComponent(_playerEntity, new T
-                {
-                    SetState = value
-                });
-            }
-        }
-
-        private void UpdateComponentWithValue<T, T1, T2>(EntityCommandBuffer ecb, InputType inputType, T1 value)
-            where T : unmanaged, IComponentData, IUserInputWithValue<T1>
-            where T1 : IUserInputValue<T2>
-        {
-            if (value.IsNotZero())
-            {
-                if (!_lastFrameInput[(int)inputType])
-                {
-                    ecb.AddComponent<T>(_playerEntity);
-                    _lastFrameInput[(int)inputType] = true;
-                }
-                ecb.SetComponent(_playerEntity, new T
-                {
-                    SetValue = value
-                });
-            }
-            else if (_lastFrameInput[(int)inputType])
-            {
-                ecb.RemoveComponent<T>(_playerEntity);
-                _lastFrameInput[(int)inputType] = false;
-            }
-        }
-
-        protected override void OnStopRunning()
-        {
-            _movementActions.Disable();
-        }
-
-        protected override void OnDestroy()
-        {
-            _lastFrameState.Dispose();
-            _lastFrameInput.Dispose();
-            base.OnDestroy();
+                ECB = SystemAPI.GetSingleton<BeginInitializationEntityCommandBufferSystem.Singleton>()
+                .CreateCommandBuffer(World.Unmanaged),
+                LastFrameState = _lastFrameState,
+                LastFrameInput = _lastFrameInput,
+                Movement = _movementActions.DefaultMap.Movement.ReadValue<Vector2>(),
+                MouseMovement = _movementActions.DefaultMap.MouseMovement.ReadValue<Vector2>(),
+                MousePosition = mousePos,
+                Ray = new Ray {Origin = managedRay.origin, Displacement = managedRay.direction},
+                MouseScroll = (int)Mouse.current.scroll.ReadValue().normalized.y,
+                SelectPressed = _movementActions.DefaultMap.Select.IsPressed(),
+                DragPressed = _movementActions.DefaultMap.Drag.IsPressed()
+            }.Schedule();
         }
     }
 }
