235de1e35580195298a075f1199635e1f207c099
Enduriel <endur1el@protonmail.com>
2024-01-06 19:17:52 +0100

    feat: make hex labels adjust for height

--- Assets/Scripts/HexGrid/Baking/HexGridCanvas.cs
+++ Assets/Scripts/HexGrid/Baking/HexGridCanvas.cs
@@ -1,34 +1,76 @@
 ﻿using TMPro;
+using Trideria.Mesh;
 using Trideria.View;
 using Unity.Entities;
+using Unity.Rendering;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace Trideria.HexGrid
 {
 	public class HexGridCanvas<T> : EntityFollower<T> where T : unmanaged, IHexGridData, IComponentData
 	{
 		public TextMeshProUGUI cellLabelPrefab;
+
+		protected TextMeshProUGUI[] _labels;
+		protected int _meshId;
+
 		protected override void TryAwake()
 		{
 			base.TryAwake();
-			if (IsAwake)
+			if (!IsAwake)
 			{
-				var grid = Manager.GetComponentData<T>(_myTarget);
-				foreach (var hexCoords in grid.GetHexes())
-				{
-					var label = Instantiate(cellLabelPrefab, transform, false);
-					label.rectTransform.anchoredPosition =
-						HexHelpers.GetRelativePosition(HexCoordinates.Zero, hexCoords).xz;
-					label.text = $"{hexCoords.Q}\n{hexCoords.R}\n{hexCoords.S}";
-				}
+				return;
+			}
+
+			if (!Manager.HasBuffer<HexBuffer>(_myTarget) || !Manager.HasComponent<MaterialMeshInfo>(_myTarget))
+			{
+				_myTarget = Entity.Null;
+				UnRegister();
+				return;
+			}
+
+			_meshId = Manager.GetComponentData<MaterialMeshInfo>(_myTarget).Mesh;
+			var grid = Manager.GetComponentData<T>(_myTarget);
+			InitLabels(grid);
+			UpdateLabels(grid);
+		}
+
+		protected virtual void InitLabels(T grid)
+		{
+			_labels = new TextMeshProUGUI[grid.GetNumHexes()];
+			for (var i = 0; i < _labels.Length; i++)
+			{
+				_labels[i] = Instantiate(cellLabelPrefab, transform, false);
+			}
+		}
+
+		protected virtual void UpdateLabels(T grid)
+		{
+			var j = 0;
+			foreach (var hex in Manager.GetBuffer<HexBuffer>(_myTarget))
+			{
+				var label = _labels[j++];
+				var pos = HexHelpers.GetRelativePosition(HexCoordinates.Zero, hex.Coords, hex.Height);
+				pos.y = -pos.y - 0.1f;
+				label.rectTransform.anchoredPosition3D = pos.xzy;
+				label.text = $"{hex.Coords.Q}\n{hex.Coords.R}\n{hex.Coords.S}";
 			}
 		}
 
 		protected override void Update()
 		{
 			base.Update();
-			transform.position += Vector3.up * 0.1f;
+			if (!IsAwake)
+			{
+				return;
+			}
+			// transform.position += Vector3.up * 0.1f;
 			transform.rotation *= Quaternion.Euler(90, 0, 0);
+			if (Manager.GetComponentData<MaterialMeshInfo>(_myTarget).Mesh != _meshId)
+			{
+				UpdateLabels(Manager.GetComponentData<T>(_myTarget));
+			}
 		}
 	}
 }
--- Assets/Scripts/HexGrid/Baking/HexHexGridCanvas.cs
+++ Assets/Scripts/HexGrid/Baking/HexHexGridCanvas.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using TMPro;
-using Trideria.View;
-using UnityEngine;
-using UnityEngine.PlayerLoop;
-
-namespace Trideria.HexGrid
+﻿namespace Trideria.HexGrid
 {
 	public class HexHexGridCanvas : HexGridCanvas<HexHexGridData>
 	{
